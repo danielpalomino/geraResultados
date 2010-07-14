88f315a71220b337e6de3e031cc15d2286a7183e
Daniel Palomino <danielmpalomino@gmail.com>
2010-07-14 10:01:19 -0300

    simulations claudio

--- src/geraResultados.py
+++ src/geraResultados.py
@@ -18,24 +18,26 @@ coderNormal = '/home/daniel/NetBeansProjects/jm-normal/bin/lencod.exe'
 coderHeuristic = '/home/daniel/NetBeansProjects/jm-heuristic/bin/lencod.exe'
 resultsFile = '../Resultados/results.csv'
 #VideoList = ['ROLLINGTOMATOES_1920x1080_25_orig_01.yuv']
-VideoList = ['STATION2_1920x1080_25_orig_01.yuv',
-'SUNFLOWER_1920x1080_25_orig_01.yuv',
-'TRACTOR_1920x1080_25_orig_01.yuv',
-'TRAFFIC_1920x1080_25_orig_01.yuv',
-'MANINCAR_1920x1080_25_orig_01.yuv',
-'PEDISTRIANAREA_1920x1080_25_orig_01.yuv',
-'RIBERBED_1920x1080_25_orig_01.yuv',
-'ROLLINGTOMATOES_1920x1080_25_orig_01.yuv',
-'RUSHHOUR_1920x1080_25_orig_01.yuv']
+#VideoList = ['STATION2_1920x1080_25_orig_01.yuv',
+#'SUNFLOWER_1920x1080_25_orig_01.yuv',
+#'TRACTOR_1920x1080_25_orig_01.yuv',
+#'TRAFFIC_1920x1080_25_orig_01.yuv',
+#'MANINCAR_1920x1080_25_orig_01.yuv',
+#'PEDISTRIANAREA_1920x1080_25_orig_01.yuv',
+#'RIBERBED_1920x1080_25_orig_01.yuv',
+#'ROLLINGTOMATOES_1920x1080_25_orig_01.yuv',
+#'RUSHHOUR_1920x1080_25_orig_01.yuv']
+video = 'TRACTOR_1920x1080_25_orig_01.yuv'
 #VideoList = ['FOREMAN_352x288_30_orig_01.yuv', 'BUS_352x288_30_orig_01.yuv', 'CITY_352x288_30_orig_01.yuv', 'FOOTBALL_352x288_30_orig_01.yuv', 'HARBOUR_352x288_30_orig_01.yuv', 'ICE_352x288_30_orig_02.yuv', 'SOCCER_352x288_30_orig_02.yuv', 'SOCCER_704x576_30_orig_02.yuv']
 ListTH = []
-ListTH.extend(range(0, 8000, 100))
+ListTH.extend(range(0, 8100, 100))
 #ListTH = [4000]
 NUM_FRAMES = 10
 ENABLE_I4MB = 1
-ENABLE_I16MB = 0
+ENABLE_I16MB = 1
 RDO_ON = 1
-QP = 28
+#QP = 28
+QPList = [12,25,38,51]
 SAD = 0
 SSE = 1
 SATD = 2
@@ -44,7 +46,7 @@ DISABLEINTRAININTER = 0
 INTRAPERIOD = 1
 NUMBEROFREFERENCEFRAMES = 4
 
-def setCfgFile(cfgFile):
+def setCfgFile(cfgFile,QP):
 	if(ENABLE_I4MB):
 		cfgFile.enableI4MB()
 	else:
@@ -126,7 +128,22 @@ def resultsIntra():
 
 	    buff1 = getNormalFileResults()
 	    for line in buff1:
-			    resultados.writeLine('Normal, ' + 'RDO,' + line)
+			    resultados.writeLine('RDO-off,SAD,16x16' + line)
+
+    resultados.createFile()
+
+def claudioSimulation():
+    resultados = ResultadosBitRatePSNR(resultsFile)
+    for qp in QPList:
+	cfgFile = CfgFile(cfgRef, video)
+	setCfgFile(cfgFile,qp)
+	resultados.writeLine(cfgFile.getVideoName() + '\n')
+	resultados.writeLine(str(qp) + ',')
+	runCoderNormal(cfgFile.getFileName())
+
+	buff1 = getNormalFileResults()
+	for line in buff1:
+	    resultados.writeLine(line)
 
     resultados.createFile()
 
@@ -143,6 +160,7 @@ def main():
 
 	    runDCT()
 	    for TH in ListTH:
+		    print 'Rodando para o Threshold ' + str(TH)
 		    resultados.writeLine(str(TH) + ',')
 		    geraOurModes(TH)
 		    runCoderHeuristic(cfg.getFileName())
@@ -153,5 +171,6 @@ def main():
     resultados.createFile()
 
 if __name__ == "__main__":
-    resultsIntra()
+    #resultsIntra()
     #main()
+    claudioSimulation()
